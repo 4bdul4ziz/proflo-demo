0b21d56f658dee02267625d86439ff89b5ba3a49
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-05-25 04:56:49 +0530

    date picker proto

---
+++ src/components/dataPicker.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Calendar} from './calendar';
+
+
+export const DatePicker: React.FC<{}> = ({}) => {
+    return (
+        <div className="date-picker-container">
+            <div className="background-procurement">
+                <div className="calendars-container"><Calendar /></div>     
+            </div>
+
+
+
+
+
+            <div className="background-sales">
+                <div className="calendars-container">
+                    <Calendar />
+                </div>     
+            </div>
+        </div>
+    )
+}
