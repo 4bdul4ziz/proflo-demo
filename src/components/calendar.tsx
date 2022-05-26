c677789e297e7f3dc507733a50fd8bcb189a11e5
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-05-26 05:41:39 +0530

    adding in days and weeks

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Weekday, Date } from '../../types'
 import {Weekdays} from '../configs/Weekdays'
+import {monthDates} from '../configs/monthDays'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useRef } from 'react'
 
 export const Calendar: React.FC<{}> = ({}) => {
+
+    const generateDates = (date: number) => {
+        for (let i = 0; i < 7; i++) {
+            return <button className="date" value={date}><p>{date}</p></button>
+        }
+    }
+
+    const generateWeeks = (dates: Array<Date>) => {
+        let daysInWeek = 7;
+        let tempArray = [];
+        for (let i = 0; i < dates.length; i += daysInWeek) {
+            tempArray.push(dates.slice(i, i + daysInWeek));
+        }
+        return tempArray;
+    }
     return (
         /*make two calendars on each side of the horizontal background
         * one for the sellers and one for the buyers */
@@ -16,7 +32,14 @@ export const Calendar: React.FC<{}> = ({}) => {
                     <div className="week-day">{day}</div>
                 ))}
             </div>
-            <div className="calendar"></div>
+            <div className="calendar">
+                {
+                    generateWeeks(monthDates).map(week => (
+                        <div className="week">{week.map(day => (generateDates(day.day)))}</div>
+                    ))
+                }
+            </div>
+
         </div>
 
     )
