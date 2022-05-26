cb5832dde6b38de9acf9ecaa65d28c44e04f2aa0
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-05-26 05:41:56 +0530

    adding in months

--- src/configs/monthDays.ts
+++ src/configs/monthDays.ts
@@ -1,5 +1,12 @@
 import { Date } from "../../types";
 
 function generateDate():Date[]{
-     
+      let dates: Date[] = [];
+        for(let i = 1; i <29; i++)
+        {
+            let date:Date = { day: i};
+            dates.push(date);
+        }
+        return dates;
 }
+export const monthDates: Date[] = generateDate();
